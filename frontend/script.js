f2b12d457dd20a7e5bb8fc9cd16f4b451800d062
mateus andrade <mateusandrade092@gmail.com>
2025-12-03 12:36:33 -0300

    ALTERAÇÕES FRONT-END: acrescentada funções de abertura de submenus e conteiner somente para analise de dados no menu de consumo e produção. Conteiner estilizado, Botões internos de menus (submenus) com função de opacidade

--- frontend/script.js
+++ frontend/script.js
@@ -10,6 +10,16 @@ function open_menu(id_btn,id_menu, displayType = "grid"){
 
 }
 
+function open_inner_option(id_btn,id_menu, displayType = "grid"){
+    const button_menu = document.getElementById(id_btn)
+    const menu_in_work = document.getElementById(id_menu)
+
+    button_menu.addEventListener("click",function(){
+        menu_in_work.style.display = displayType
+    })
+
+}
+
 function close_menu(id_menu){
     const icon_close = document.querySelectorAll(".close_windows")
     const menu_in_work = document.getElementById(id_menu)
@@ -23,8 +33,33 @@ function close_menu(id_menu){
     })
 }
 
+function opacity_btns(class_btn,id_menu){
+    const btn_in_use = document.querySelectorAll(`#${id_menu} .${class_btn}`)
+
+    btn_in_use.forEach(btn=>{
+        btn.addEventListener("click", function(){
+            btn_in_use.forEach(btn_out_use => {
+                if(btn_out_use !== btn){
+                    btn_out_use.style.opacity = "0.5"
+                }else{
+                    btn_out_use.style.opacity = "1"
+                }
+            });
+        })
+    })
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     open_menu("data_prod_cons_btn","production_and_consumption_menu", "grid")
+        opacity_btns("btn_prod_consump_container", "conteiner_production")
+        open_inner_option("potential_btn_prod_energ", "conteiner_data_analysis_consup_prod", "grid")
+        open_inner_option("util_btn_prod_energ", "conteiner_data_analysis_consup_prod", "grid")
+        open_inner_option("total_btn_prod_energ", "conteiner_data_analysis_consup_prod", "grid")
+
+        open_inner_option("consumption_total_btn_energ_data", "conteiner_data_analysis_consup_prod", "grid")
+        open_inner_option("consumpt_for_sectors_btn_energ_data", "conteiner_data_analysis_consup_prod", "grid")
+        open_inner_option("consumpt_for_resident_btn_energ_data", "conteiner_data_analysis_consup_prod", "grid")
+
     open_menu("data_reports_btn","reports_menu", "block")
     open_menu("data_panels_btn","panels_menu", "flex")
     open_menu("management_energ_btn","energetic_management", "block")
