a228dd0c7d83e3ec13d7ee98efb8665ba44ad648
mateus andrade <mateusandrade092@gmail.com>
2025-12-14 20:36:43 -0300

    ALTERAÇÃO: CALCULO CORRETO EM RELAÇÃO AO CONSUMO E PRODUÇÃO DE ENERGIA SOLAR! GRÁFICO COERENTE AOS DADOS.

--- backend/app.py
+++ backend/app.py
@@ -42,7 +42,7 @@ def production_potential():
 
     for irrad in range(len(time)):
         if time[irrad].startswith(today):
-            potencial_generated = (irrads[irrad] * pannel_area * efic) / 1000
+            potencial_generated = ((irrads[irrad] * pannel_area * efic) / 1000)*30000
             hours.append(time[irrad][-5:])
             values.append(potencial_generated)
 
@@ -57,41 +57,35 @@ def production_potential():
 
                                                         #Acima temos a função de produção de energia. De modo detalhado funciona da seguinte forma: do bloco 1 (#b1) acima temos a requisição da função clima que tem a chave API necessária para o funcionamento do sistema, colocada em uma variavel assim e sendo tratada para pegar dados especificos como a hora, e a direção da radiação solar, a variavel today serve como parametro para garantir que o sistema pegue somente o dia atual. O bloco 2 temos um loop for calculando a produção estimada com os parametros de area do painel, a hora do dia para saber a radiação e a potencia gerada e tudo isso é retornado ao front-end em forma de JSON.
 #===========================================================================================================================
-
 @app.get("/consumption_daily")
-def build_hours():
-    return [f"{h:02d}:00" for h in range(24)]
-
 def consumption_energ():
+    TOTAL_HOUSES = 2000
 
-
+    hours = [f"{h:02d}:00" for h in range(24)]
     consumption_for_day = []
-    hours =  hours = build_hours()
 
-
-
-    
     for hour in range(24):
 
         if 0 <= hour < 5:
-            consumption_for_day.append(round(random.uniform(0,0.8),2))
-            #dawn_consumption = 0 - 0.8
+            base = random.uniform(0, 0.8)
 
         elif 5 <= hour < 12:
-            consumption_for_day.append(round(random.uniform(3, 4.5),2))
-            #morning_consumption =
+            base = random.uniform(3, 4.5)
 
         elif 12 <= hour < 18:
-            consumption_for_day.append(round(random.uniform(3, 4.5),2))
-            #afternoon_consumption =  3.0 - 4.5
+            base = random.uniform(3, 4.5)
 
-        elif 18 <= hour < 24:
-            consumption_for_day.append(round(random.uniform(2, 3.9),2))
-            #nigth_consumption = 2 - 3.9
+        else:  # 18–23
+            base = random.uniform(2, 3.9)
 
+        consumption_for_day.append(
+            round(base * TOTAL_HOUSES, 2)
+        )
 
-    return{"horas":hours ,"valores":consumption_for_day}
-
+    return {
+        "horas": hours,
+        "valores": consumption_for_day
+    }
                                                                     #Acima temos um loop for, que estabelece um range entre 0 a 24, isso para as horas, e a cada hora é gerado uma estimativa de consumo de energia que pode crescer ou diminuir a depender da hora do dia. Os dados são retornados ao JSON.
 
 #===========================================================================================================================
@@ -107,6 +101,13 @@ def consumption_vs_production():
             "consumo": consumo["valores"],
             "producao": producao["valores"]
     }
+
+#===========================================================================================================================
+
+@app.get("/shipping_unic")
+def shipping_unic_invoice():
+    request.json()
+
 
 
 if __name__ == "__main__":
