b01501beb7083de5031cbde06542b0deedb7c43c
mateus andrade <mateusandrade092@gmail.com>
2026-01-06 21:24:55 -0300

    ALTERAÇÃO: alterando as variaveis locais do banco de dados para variaveis do supabase

--- backend/app.py
+++ backend/app.py
@@ -16,13 +16,7 @@ app = Flask(__name__)
 CORS(app)
 
 def get_db_connection():
-    conn = psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD")
-    )
+    conn = psycopg2.connect(os.getenv("DATABASE_URL"), sslmode="require")
     return conn
 
 @app.route("/")
